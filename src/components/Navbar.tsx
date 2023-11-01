b1c8bd37fcb9cfbdbfd0a3ada8336b4a1ae45443
unknown <142760953+scentier@users.noreply.github.com>
2023-11-01 15:46:39 +0700

    navbar phase 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
   const menu = [
     {
@@ -21,9 +24,28 @@ export default function Navbar() {
       name: "Contact",
     },
   ];
+
   return (
     <nav className="bg-gray-100 text-neutral-500 dark:text-neutral-300 dark:bg-neutral-300">
-      <div className="max-w-[700px] lg:max-w-[960px] mx-auto px-7 ">Navbar</div>
+      <div className="max-w-[700px] lg:max-w-[960px] mx-auto px-7 py-3 flex flex-col sm:flex-row sm:justify-between">
+        <Image
+          src="/next.svg"
+          alt="Next.js logo"
+          className="dark:invert sm:w-1/4"
+          width={100}
+          height={24}
+          priority
+        />
+        <div className="sm:w-3/4">
+          <ul className="flex flex-col items-end sm:flex-row sm:justify-end">
+            {menu.map((link) => (
+              <li key={link.id}>
+                <Link href={link.url}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
