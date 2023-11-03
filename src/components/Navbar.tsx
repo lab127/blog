503e096a422f7ac0caeb16fffbab5638eac7d9b0
unknown <142760953+scentier@users.noreply.github.com>
2023-11-03 10:09:48 +0700

    icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function Navbar() {
   const [isHidden, setIsHidden] = useState(true);
@@ -28,31 +29,43 @@ export default function Navbar() {
     },
   ];
 
+  console.log(isHidden);
   return (
-    <nav className="bg-gray-100 text-neutral-500 dark:text-neutral-300 dark:bg-black border-b-[1px] border-gray-800">
-      <div className="max-w-[700px] lg:max-w-[960px] mx-auto px-7 py-4 flex flex-col sm:flex-row sm:justify-between">
-        <Image
-          src="/next.svg"
-          alt="Next.js logo"
-          className="dark:invert sm:w-1/4"
-          width={100}
-          height={24}
-          priority
-        />
-        <div className="sm:w-3/4">
-          <button type="submit" onClick={() => setIsHidden(!isHidden)}>
-            toggle
+    <>
+      <nav className="bg-gray-100 text-neutral-500 dark:text-neutral-300 dark:bg-black dark:border-b-[1px] dark:border-gray-800">
+        <div className="max-w-3xl lg:max-w-5xl mx-auto px-7 py-4 flex justify-between">
+          <Image
+            src="/next.svg"
+            alt="Next.js logo"
+            className="dark:invert md:w-1/4"
+            width={100}
+            height={24}
+            priority
+          />
+          <button
+            className="md:w-3/4 md:hidden"
+            type="submit"
+            onClick={() => {
+              setIsHidden(!isHidden);
+            }}
+          >
+            {isHidden ? <AiOutlineMenu /> : <AiOutlineClose />}
           </button>
-          <ul className="flex flex-col items-end sm:flex-row sm:justify-end">
-            {menu.map((link) => (
-              <li key={link.id}>
-                <Link href={link.url}>{link.name}</Link>
-              </li>
-            ))}
-            <li className={`${isHidden && "hidden"}`}>test</li>
-          </ul>
         </div>
+      </nav>
+      <div className="flex justify-end max-w-[700px] lg:max-w-[960px] mx-auto">
+        <ul
+          className={`${
+            isHidden && "hidden"
+          } md:flex absolute bg-gray-200 md:bg-gray-100 md:mt-4 mt-8 top-0`}
+        >
+          {menu.map((link) => (
+            <li key={link.id} className="px-2">
+              <Link href={link.url}> {link.name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-    </nav>
+    </>
   );
 }
