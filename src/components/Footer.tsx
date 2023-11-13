42e6828cd2a3e8b68847a3a6e1398e361f1d35ea
unknown <142760953+scentier@users.noreply.github.com>
2023-11-13 15:12:40 +0700

    container

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import DarkModeBtn from "./DarkModeBtn";
 
 export default function Footer() {
   return (
-    <div className="max-w-3xl lg:max-w-5xl mx-auto px-7 mt-14">
+    <div className="max-w-3xl lg:max-w-5xl mx-auto py-4 px-7 mt-14 mb-5">
       <div className="flex justify-end">
         <DarkModeBtn />
       </div>
