89a0b2675df30f0e14ad9140c67ee8de41fef3b9
unknown <12640394+lab127@users.noreply.github.com>
2023-11-17 16:01:45 +0700

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,26 +33,30 @@ const menu = [
 ];
 
 export default function Navbar() {
-  const [isHidden, setIsHidden] = useState(true);
-  const [isDesktop, setDesktop] = useState(
-    typeof window !== "undefined" && window.innerWidth >= 1024
-  );
+  const [isHidden, setIsHidden] = useState<boolean>();
+  const [isDesktop, setIsDesktop] = useState<boolean>();
   const ref = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
-    const updateMedia = () => {
-      setDesktop(window.innerWidth >= 1024);
-      if (isDesktop) {
-        setIsHidden(false);
-      } else {
-        setIsHidden(true);
-      }
-    };
+    setIsHidden(window.innerWidth < 1024);
+    setIsDesktop(window.innerWidth >= 1024);
+  }, []);
 
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  }, [isDesktop]);
+  // useEffect(() => {
+  //   setScreenSize(window.innerWidth);
+  //   const updateScreenSize = () => {
+  //     if (screenSize && screenSize >= 1024) {
+  //       setIsHidden(false);
+  //     } else {
+  //       setIsHidden(true);
+  //     }
+  //   };
 
+  //   window.addEventListener("resize", updateScreenSize);
+  //   return () => window.removeEventListener("resize", updateScreenSize);
+  // }, [screenSize]);
+
+  console.log("ishidden", isHidden);
   return (
     <header>
       <nav className="z-10 fixed left-0 top-0 w-full border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
@@ -69,7 +73,6 @@ export default function Navbar() {
                 priority
               />
             </Link>
-            {isDesktop ? <div>DESKTOP</div> : <div>MOBILE</div>}
             <button
               className="lg:hidden"
               type="submit"
@@ -87,20 +90,23 @@ export default function Navbar() {
           <div
             style={{
               height: !isHidden
-                ? `${ref?.current?.getBoundingClientRect().height}px`
+                ? `${
+                    ref.current && ref.current.getBoundingClientRect().height
+                  }px`
                 : "0px",
             }}
-            className="lg:h-auto transition-all duration-500 overflow-y-hidden overflow-auto my-2"
+            className="transition-all duration-500 overflow-y-hidden overflow-auto my-2"
           >
-            <ul
-              ref={ref}
-              // className={`flex flex-col lg:flex-row bg-red-300 bg-inherit lg:my-3 lg:mx-0 mt-14 mx-2  bg-gradient-to-t drop-shadow-xl`}
-            >
+            <ul ref={ref} className={`h-auto flex flex-col lg:flex-row`}>
               {menu.map((link) => (
                 <li key={link.id} className="lg:px-2 px-4 py-1">
                   <Link
                     onClick={() => {
-                      setIsHidden(!isHidden);
+                      if (isDesktop) {
+                        setIsHidden(false);
+                      } else {
+                        setIsHidden(!isHidden);
+                      }
                     }}
                     className="text-zinc-800 dark:text-zinc-400 hover:text-pink-700"
                     href={link.url}
